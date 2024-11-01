18d47dec2c194d517325be792219f76a69545638
Brent Ellis <brent.ellis@gmail.com>
2024-11-01 08:40:14 -0500

    Add files via upload

---
+++ copilot_entra_createSelfSignedCertificate.ps1
@@ -0,0 +1,17 @@
+﻿<#
+ Microsoft provides programming examples for illustration only, without warranty either expressed or
+ implied, including, but not limited to, the implied warranties of merchantability and/or fitness 
+ for a particular purpose. 
+#>
+
+# Reference: https://learn.microsoft.com/en-us/entra/identity-platform/howto-create-self-signed-certificate
+
+$certname = "{certificateName}"    ## Replace {certificateName}
+$cert = New-SelfSignedCertificate -Subject "CN=$certname" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
+Export-Certificate -Cert $cert -FilePath "C:\temp\$certname.cer"   ## Specify your preferred location
+
+# Upload to the certificates section of your app registration
+
+# OPTIONAL DELETE
+# Get-ChildItem -Path "Cert:\CurrentUser\My" | Where-Object {$_.Subject -Match "$certname"} | Select-Object Thumbprint, FriendlyName
+# Remove-Item -Path Cert:\CurrentUser\My\{pasteTheCertificateThumbprintHere} -DeleteKey
